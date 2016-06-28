bcff13b8d686174fd55c7d2722b9ef60d0c112fc
Walter Mauritz <wrsmau@Walters-MacBook-Pro.local>
2016-06-28 15:03:48 +0200

    rename

---
+++ app/controllers/passenger-controller.js
@@ -0,0 +1,20 @@
+﻿app.controller("passengerCtrl", function($http, passengerService) {
+    var that = this;
+
+    that.selection;
+
+    that.search = function() {
+        var name = that.filterName;
+        passengerService.search(name).then(function(passengers) {
+            that.passengers = passengers;
+        })
+    }
+
+    that.select = function(passenger) {
+        that.selection = passenger;
+        console.log("passenger selected with id: " + passenger.id);
+    }
+
+    that.passengers = that.search("");
+
+} );
